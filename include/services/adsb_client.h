7bb4c50d0584f5295d553edc2f19fe64b071739e
xUser-HA <73344046+xUser-HA@users.noreply.github.com>
2026-09-16 00:07:44 +0500

    Prepare ADS-B async update

--- include/services/adsb_client.h
+++ include/services/adsb_client.h
@@ -22,9 +22,13 @@ const Aircraft* aircraftList();
 
 /** Hook invoked during long HTTP I/O (e.g. wifiLoop). Optional. */
 using PollFn = void (*)();
+
 void setPollFn(PollFn fn);
 
 /** Fetch aircraft within fetch_radius_km of center_lat/lon from adsb.fi. */
-bool fetchUpdate(double center_lat, double center_lon, float fetch_radius_km);
+bool fetchUpdate(
+    double center_lat,
+    double center_lon,
+    float fetch_radius_km);
 
 }  // namespace services::adsb
